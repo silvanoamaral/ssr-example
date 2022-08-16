9f5beeb4eed0b0021da2d793ea310969b0fab1a1
silvanoamaral <silvano.amaral9@gmail.com>
2022-08-16 08:55:51 -0300

    add form

--- src/App.js
+++ src/App.js
@@ -2,22 +2,40 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = React.useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
   return (
-    <>
-      <p>{count}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-    </>
+    <div className="form-container">
+      <div className="field-container">
+        <label htmlFor="name">Nome</label>
+        <input id="name" maxLength="20" type="text" />
+      </div>
+      <div className="field-container">
+        <label htmlFor="cardnumber">Número do cartão</label>
+        <input
+          id="cardnumber"
+          type="text"
+          pattern="[0-9]*"
+          inputMode="numeric"
+        />
+      </div>
+      <div className="field-container">
+        <label htmlFor="expirationdate">Expiração (mm/yy)</label>
+        <input
+          id="expirationdate"
+          type="text"
+          pattern="[0-9]*"
+          inputMode="numeric"
+        />
+      </div>
+      <div className="field-container">
+        <label htmlFor="securitycode">Código de segurança</label>
+        <input
+          id="securitycode"
+          type="text"
+          pattern="[0-9]*"
+          inputMode="numeric"
+        />
+      </div>
+    </div>
   );
 }
 
